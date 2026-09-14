33885722ec442bb0b4da87bd10397cfe12748c97
lucasmarques2907 <lucasredra@gmail.com>
2026-09-14 18:20:35 -0300

    fix: map z-index above navbar

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -18,7 +18,7 @@ export default function LocationMap() {
       doubleClickZoom={false}
       touchZoom={false}
       keyboard={false}
-      className='bg-surface0 h-full w-full'
+      className='bg-surface0 h-full w-full z-0'
     >
       <TileLayer
         url={`https://basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png?key=${process.env.NEXT_PUBLIC_CARTO_KEY}`}
